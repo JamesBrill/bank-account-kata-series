da458c29d6695df45d69eac515b290a2743e81bf
JamesBrill <james.brill@red-gate.com>
2015-06-09 14:38:54 +0100

    Green: Zero value equality

--- BankingKata/Money.cs
+++ BankingKata/Money.cs
@@ -2,11 +2,44 @@
 
 namespace BankingKata
 {
-    public class Money
+    public class Money : IEquatable<Money>
     {
+        private readonly int _value;
+
         public Money(int value)
         {
+            _value = value;
             if (value < 0) throw new ArgumentOutOfRangeException("value", value, "Value must be non-negative");
+        }
+
+        public bool Equals(Money other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return _value == other._value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Money) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return _value;
+        }
+
+        public static bool operator ==(Money left, Money right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Money left, Money right)
+        {
+            return !Equals(left, right);
         }
     }
 }
